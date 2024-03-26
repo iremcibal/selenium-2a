24f662220a477bf50a8838de39b3a8c3c0b91c0a
irem-balci <iremcibal@gmail.com>
2024-03-26 11:43:51 +0300

    son part

--- test_demo.py
+++ test_demo.py
@@ -27,7 +27,10 @@ class Test_Demo:
         text = "Hello"
         assert text == "Hello"
 
-    @pytest.mark.parametrize("username,password",[("1","1"),("abc","123"),("deneme","secret_sauce")])
+    def getData():
+        return [("1","1"),("abc","123"),("deneme","secret_sauce")]
+    
+    @pytest.mark.parametrize("username,password",getData())
     def test_invalid_login(self,username,password):
         userNameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
         passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
